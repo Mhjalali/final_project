77753a6caff34c14ee8b83f25d2e0bab1678ec21
mohammad hossein jalali <56398549+Mhjalali@users.noreply.github.com>
2020-09-03 01:23:16 +0430

    Add files via upload

--- dbserver.c
+++ dbserver.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <libpq-fe.h>
 #include <dirent.h>
+#include "libpq-fe.h"
 #include "string.h"
+
 char files[60][30];
 
 int listFiles() {
     struct dirent *dp;
-    DIR *dir = opendir("\\tmp\\final_project\\");
-
+    DIR *dir = opendir("C:\\Users\\MHJ\\Desktop\\new");
     if (!dir)
-        return;
+        return 0;
 
     int i = 0;
     while ((dp = readdir(dir)) != NULL) {
@@ -34,41 +34,52 @@ void do_exit(PGconn *conn, PGresult *res) {
 
 
 int main() {
-    int i,j,k;
+    int i, j, k;
     int n = listFiles();
-
     PGconn *conn = PQsetdbLogin("localhost",
                                 "5432", "", "",
                                 "fpdb", "postgres", "");
-
     if (PQstatus(conn) == CONNECTION_BAD) {
         fprintf(stderr, "Connection to database failed: %s\n",
                 PQerrorMessage(conn));
     }
-
     for (i = 2; i < n; ++i) {
-        char s[70] = "\\tmp\\final_project\\";
-        char param[8][20];
-        FILE *ftp = fopen(strcat(s,files[i]),"r");
-        while (!feof(ftp)){
+        char s[70] = "C:\\Users\\MHJ\\Desktop\\new\\";
+        char param[8][40];
+        FILE *ftp = fopen(strcat(s, files[i]), "r");
+        while (!feof(ftp)) {
             char buff[200];
             fgets(buff, 200, ftp);
-            char * token = strtok(buff, ",");
+            char *token = strtok(buff, ",");
             int i = 0;
-            while( token != NULL ) {
+            while (token != NULL) {
                 strcpy(param[i], token);
                 i++;
                 token = strtok(NULL, ",");
             }
-            for (int l = 0; l < i; ++l) {
-
+            i--;
+            int l;
+            for (l = 0; l < i+1; ++l) {
+                if (l == i ) {
+                    strcat(param[l], "');");
+                } else {
+                    strcat(param[l], "','");
+                }
             }
-            res = PQexec(conn, "INSERT INTO fp_stores_data VALUES(,'Audi',52642)");
+
+            char order[300] = "INSERT INTO fp_stores_data (time, province, city, market_id, product_id, price, quantity, has_sold) VALUES('";
+
+            for (l = 0; l < i+1; ++l) {
+                strcat(order, param[l]);
+            }
+
+            PGresult *res = PQexec(conn, order);
 
             if (PQresultStatus(res) != PGRES_COMMAND_OK)
                 do_exit(conn, res);
 
             PQclear(res);
+
         }
         fclose(ftp);
     }
